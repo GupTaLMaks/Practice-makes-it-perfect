4fda2a3b4e424e1ac2ca9cf5c5759bf25b86c1b9
Paul Leroy <99101923+GupTaLMaks@users.noreply.github.com>
2024-03-19 23:14:58 +0100

    Add files via upload

---
+++ Countdown/Countdown.js
@@ -0,0 +1,53 @@
+window.addEventListener("DOMContentLoaded",function () {
+    let countdown;
+    const timerDisplay = document.querySelector('.display__time-left');
+    const timerButtons = document.querySelectorAll('[data-time]');
+    const endTime = document.querySelector('.display__end-time');
+
+    function timer(seconds) {
+        clearInterval(countdown);
+        const now = Date.now();
+        const then = now + seconds * 1000;
+        displayTimeLeft(seconds);
+        displayEndTime(then);
+
+        countdown = setInterval(() => {
+            const secondsLeft = Math.round((then - Date.now()) / 1000);
+            if (secondsLeft < 0) {
+                clearInterval(countdown);
+                return;
+            }
+            displayTimeLeft(secondsLeft);
+        },1000);
+    }
+
+    function displayTimeLeft(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        const remainderMinutes = seconds % 3600;
+        const minutes = Math.floor(remainderMinutes / 60);
+        const remainderSeconds = remainderMinutes % 60;
+        const display = `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+        document.title = display;
+        timerDisplay.textContent = display;
+    }
+
+    function displayEndTime(timestamp) {
+        const end = new Date(timestamp);
+        const hour = end.getHours();
+        const minutes = end.getMinutes();
+        endTime.textContent = `Be Back At ${hour < 10 ? '0' : ''}${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+    }
+
+    function startTimer(){
+        const seconds = parseInt(this.dataset.time);
+        timer(seconds);
+    }
+    timerButtons.forEach(timerButton => timerButton.addEventListener('click', startTimer));
+
+    document.customForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        const mins = this.minutes.value;
+        this.reset();
+        timer(mins*60);
+    })
+});
